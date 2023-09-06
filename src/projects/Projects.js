af4422b9eb5a73c428fbd1695bb205e40fc2dec9
Inna Groznaya <inna2011@tut.by>
2023-09-06 18:53:00 +0300

    added some visual feaches to project

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -3,8 +3,9 @@ import style from './Projects.module.scss';
 import styleContainer from '../common/styles/Container.module.css'
 import Project from "./project/Project";
 import Title from "../common/components/title/Title";
-import todoImage from "./../assets/image/mockup_to_do_list.png"
-import socialImage from "./../assets/image/socnet.png"
+import todoImage from "./../assets/image/mockup_to_do_list.png";
+import socialImage from "./../assets/image/socnet.png";
+import Fade from "react-reveal/Fade";
 
 function  Projects() {
     const social = {
@@ -17,10 +18,12 @@ function  Projects() {
         <div id={"projects"} className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
                 <Title text={'projects'}/>
+                <Fade bottom>
                 <div className={style.projects}>
                     <Project style={social} title={'Social network'} description={'Short description'}/>
                     <Project style={todolist} title={'Todo list'} description={'Short description'}/>
                 </div>
+                </Fade>               
             </div>
         </div>
     );
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -3,12 +3,14 @@ import style from './Skills.module.scss';
 import styleContainer from '../common/styles/Container.module.css'
 import Skill from "./skill/Skill";
 import Title from "../common/components/title/Title";
+import Fade from "react-reveal/Fade";
 
 function Skills() {
     return (
         <div id={"skills"} className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <Title text={'skills'}/>
+                <Fade>
                 <div className={style.skills}>
                     <Skill title={'JS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>
                     <Skill title={'TS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>
@@ -18,6 +20,7 @@ function Skills() {
                     <Skill title={'REDUX'} description={' Animi distinctio excepturi iusto praesentium quos saepe? Distinctio exercitationem impedit natus omnis vel?'}/>
                     <Skill title={'ENGLISH'} description={' Animi distinctio excepturi iusto praesentium quos saepe? Distinctio exercitationem impedit natus omnis vel?'}/>
                 </div>
+                </Fade>
             </div>
         </div>
     );
